6430cd1ba8edcd7176ac804d8b6a98bd23a2e96d
umesh2407 <choudharyumesh986@gmail.com>
2024-06-30 22:24:54 +0530

    added all user controllers and routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,3 +10,104 @@ exports.getUserDetails = async (req, res) => {
     });
   };
 
+// update User Profile
+exports.updateProfile = async (req, res) => {
+    const newUserData = {
+      name: req.body.name,
+      email: req.body.email,
+    };
+  
+    // if (req.body.avatar !== "") {
+    //   const user = await User.findById(req.user.id);
+  
+    //   const imageId = user.avatar.public_id;
+  
+    //   await cloudinary.v2.uploader.destroy(imageId);
+  
+    //   const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    //     folder: "avatars",
+    //     width: 150,
+    //     crop: "scale",
+    //   });
+  
+    //   newUserData.avatar = {
+    //     public_id: myCloud.public_id,
+    //     url: myCloud.secure_url,
+    //   };
+    // }
+  
+    const user = await User.findByIdAndUpdate(req.user.user, newUserData, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+       message: "Profile updated succesfully"
+    });
+  };
+  
+  // Get all users(admin)
+  exports.getAllUser = async (req, res) => {
+    const users = await User.find();
+  
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  };
+  
+  // Get single user (admin)
+  exports.getSingleUser = async (req, res) => {
+    const user = await User.findById(req.params.id);
+  
+    if (!user) {
+           return res.status(400).json({ message: "User does not exist with Id" });
+    }
+  
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  };
+  
+  // Update User Role -- Admin
+  exports.updateUserRole = async (req, res) => {
+    const newUserData = {
+      name: req.body.name,
+      email: req.body.email,
+      role: req.body.role,
+    };
+  
+    await User.findByIdAndUpdate(req.params.id, newUserData, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+         message: "Updated User Role successfully"
+      });
+  };
+  
+  // Delete User --Admin
+  exports.deleteUser = async (req, res) => {
+    const user = await User.findById(req.params.id);
+  
+    if (!user) {
+        return res.status(400).json({ message: "User does not exist with Id" });
+    }
+  
+    // const imageId = user.avatar.public_id;
+  
+    // await cloudinary.v2.uploader.destroy(imageId);
+  
+    await user.deleteOne();
+  
+    res.status(200).json({
+      success: true,
+      message: "User Deleted Successfully",
+    });
+  };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
-const { verifyToken } = require("../middlewares/auth");
-
+const { verifyToken ,  authorizeRoles } = require("../middlewares/auth");
 
 router.get("/me", verifyToken ,userController.getUserDetails);
+router.put("/me/update", verifyToken ,userController.updateProfile);
+router.get("/getall", verifyToken , authorizeRoles("admin") ,userController.getAllUser); //only Admin can use 
+router.get("/:id", verifyToken ,userController.getSingleUser); 
+router.put('/update/role/:id', verifyToken, authorizeRoles('admin'),userController.updateUserRole);
+router.delete("/:id", verifyToken, authorizeRoles('admin'),userController.deleteUser); 
 
 module.exports = router;
