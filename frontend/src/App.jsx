49a67aefde5eea4fe151d274e98bb4aa9cba0e75
umesh2407 <choudharyumesh986@gmail.com>
2024-07-01 15:45:58 +0530

    added Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from './components/Header/Header';
+import Webfont from 'webfontloader';
+import Footer from './components/Footer/Footer';
+
 
 const App = () => {
+  
+React.useEffect(()=>{
+  Webfont.load({
+    google: {
+      families: ["Roboto","Droid Sans", "Chilanka"]
+    }
+  })
+});
+
   return (
     <Router>
  <>
  <Header/>
- 
+<Footer/>
  </>
     </Router>
   )
