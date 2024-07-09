4d7cff4c5f3489e3adc1c42fe34d3da133a21d5a
umesh2407 <choudharyumesh986@gmail.com>
2024-07-09 12:52:36 +0530

    added responsiveness

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -36,17 +36,17 @@ const Header = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center">
             <Link to="/">
-              <img src="/logo.png" alt="E-Shop Logo" className="w-[160px]" />
+              <img src="/logo.png" alt="E-Shop Logo" className="w-[150px]" />
             </Link>
           </div>
-          <div>
+          <div className="flex-grow hidden md:flex justify-center ml-5">
             <SearchInput />
           </div>
-          {/* Primary Nav */}
-          <div className="hidden md:flex items-center space-x-4">
+          {/* Primary Nav (Desktop) */}
+          <div className="hidden md:flex items-center space-x-5">
             <Link
               to="/"
-              className="py-5 px-3 text-gray-900 hover:text-gray-700 hover:underline font-bold"
+              className="py-5 px-3 text-gray-900 hover:text-gray-700 hover:underline font-bold ml-2"
             >
               Home
             </Link>
@@ -59,59 +59,60 @@ const Header = () => {
                 Cart
               </Link>
             </Badge>
-       
-              <Link
-                to="/wishlist"
-                className="py-5 px-6 text-gray-900 hover:text-gray-700 hover:underline font-bold"
-              >
-                <FaRegHeart />
-              </Link>
-        
+
+            <Link
+              to="/wishlist"
+              className="py-5 px-3 text-gray-900 hover:text-gray-700 hover:underline font-bold"
+            >
+              <FaRegHeart />
+            </Link>
           </div>
 
-          {!auth.user ? (
-            <div className="hidden md:flex items-center">
+          {/* User Navigation */}
+          <div className="flex items-center">
+            {!auth.user ? (
               <Link
                 to="/login"
-                className="py-5 px-3 text-gray-900 hover:text-gray-700"
+                className="hidden md:block py-5 px-3 text-gray-900 hover:text-gray-700"
               >
                 <FaRegUserCircle className="h-7 w-6" />
               </Link>
-            </div>
-          ) : (
-            <div className="hidden md:flex items-center relative">
-              <button
-                onClick={() => setShowDropdown(!showDropdown)}
-                className="py-5 px-3 text-gray-700 hover:text-gray-900 flex items-center font-bold"
-              >
-                {auth?.user?.name}
-                <FaCaretDown className="ml-2" />
-              </button>
-              {showDropdown && (
-                <div className="absolute right-0 mt-8 w-48 bg-white border rounded shadow-lg font-bold">
-                  <Link
-                    to={`/dashboard/${
-                      auth?.user?.role === "admin" ? "admin" : "user"
-                    }`}
-                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    Dashboard
-                  </Link>
-                  <button
-                    onClick={handleLogOut}
-                    className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    <IoLogOut className="inline mr-2" />
-                    LogOut
-                  </button>
-                </div>
-              )}
-            </div>
-          )}
+            ) : (
+              <div className="hidden md:flex items-center relative">
+                <button
+                  onClick={() => setShowDropdown(!showDropdown)}
+                  className="py-5 px-3 text-gray-700 hover:text-gray-900 flex items-center font-bold"
+                >
+                  {auth?.user?.name}
+                  <FaCaretDown className="ml-2" />
+                </button>
+                {showDropdown && (
+                  <div className="absolute right-0 mt-8 w-48 bg-white border rounded shadow-lg font-bold">
+                    <Link
+                      to={`/dashboard/${
+                        auth?.user?.role === "admin" ? "admin" : "user"
+                      }`}
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      Dashboard
+                    </Link>
+                    <button
+                      onClick={handleLogOut}
+                      className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      <IoLogOut className="inline mr-2" />
+                      LogOut
+                    </button>
+                  </div>
+                )}
+              </div>
+            )}
 
-          {/* Mobile Button */}
-          <div className="md:hidden flex items-center">
-            <button className="mobile-menu-button" onClick={toggleMenu}>
+            {/* Mobile Button (Hamburger Menu) */}
+            <button
+              className="md:hidden ml-2 text-gray-900 hover:text-gray-700 focus:outline-none"
+              onClick={toggleMenu}
+            >
               <svg
                 className="w-6 h-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -132,45 +133,53 @@ const Header = () => {
       </div>
 
       {/* Mobile Menu */}
-      <div className={`mobile-menu ${isOpen ? "block" : "hidden"} md:hidden`}>
-        <Link to="/" className="block py-2 px-4 text-sm hover:bg-gray-200">
-          Home
-        </Link>
-        <Link to="/cart" className="block py-2 px-4 text-sm hover:bg-gray-200">
-          Cart ({cart?.length})
-        </Link>
-        <Link
-          to="/wishlist"
-          className="block py-2 px-4 text-sm hover:bg-gray-200"
-        >
-          <FaRegHeart className="inline mr-2" />
-          Wishlist ({wishlist?.length})
-        </Link>
-        {!auth.user ? (
+      <div className={`md:hidden ${isOpen ? "block" : "hidden"}`}>
+        <div className="px-2 pt-2 pb-3 space-y-1">
           <Link
-            to="/login"
-            className="block py-2 px-4 text-sm hover:bg-gray-200"
+            to="/"
+            className="block py-2 px-4 text-sm text-gray-900 hover:bg-gray-200"
           >
-            <FaRegUserCircle className="inline mr-2" />
-            Login
+            Home
           </Link>
-        ) : (
-          <>
+          <Link
+            to="/cart"
+            className="block py-2 px-4 text-sm text-gray-900 hover:bg-gray-200"
+          >
+            Cart ({cart?.length})
+          </Link>
+          <Link
+            to="/wishlist"
+            className="block py-2 px-4 text-sm text-gray-900 hover:bg-gray-200"
+          >
+            <FaRegHeart className="inline mr-2" />
+            Wishlist ({wishlist?.length})
+          </Link>
+          {!auth.user ? (
             <Link
-              to="/dashboard"
-              className="block py-2 px-4 text-sm hover:bg-gray-200"
+              to="/login"
+              className="block py-2 px-4 text-sm text-gray-900 hover:bg-gray-200"
             >
-              Dashboard
+              <FaRegUserCircle className="inline mr-2" />
+              Login
             </Link>
-            <button
-              onClick={handleLogOut}
-              className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-gray-100"
-            >
-              <IoLogOut className="inline mr-2" />
-              LogOut
-            </button>
-          </>
-        )}
+          ) : (
+            <>
+              <Link
+                to="/dashboard"
+                className="block py-2 px-4 text-sm text-gray-900 hover:bg-gray-200"
+              >
+                Dashboard
+              </Link>
+              <button
+                onClick={handleLogOut}
+                className="w-full text-left block px-4 py-2 text-gray-700 hover:bg-gray-100"
+              >
+                <IoLogOut className="inline mr-2" />
+                LogOut
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
